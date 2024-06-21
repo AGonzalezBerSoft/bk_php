4f5d639bc5cccd33d9a8c9908a0a3fdce3e513f7
Anyel Gonzalez <agonzalez@bersoft.co>
2024-06-21 16:49:31 -0500

    fix

--- database.php
+++ database.php
@@ -26,28 +26,16 @@ class database {
             if (file_exists("$file.gz")) {
                 unlink("$file.gz");
             }
-            if($this->compress($file, "$file.gz")) {
-                unlink($file);
+            $command = "gzip --force $file";
+            exec($command, $output, $return_var);
+            if ($return_var !== 0) {
+                $response->msg = "There was a problem backing up the database.";
+            } else {
                 $response->success = true;
                 $response->file = "$file.gz";
-            };
+            }
         }
         return $response;
-    }
-    private function compress($file, $gzfile) {
-        try {
-            $fp = fopen($file, 'rb');
-            $data = fread($fp, filesize($file));
-            fclose($fp);
-    
-            $zp = gzopen($gzfile, 'w9');
-            gzwrite($zp, $data);
-            gzclose($zp);
-            return true;
-        } catch (\Throwable $th) {
-            print_r($th);
-            return false;
-        }
     }
 }
 
